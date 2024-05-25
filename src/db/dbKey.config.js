1807fba83f9a623ef1f10822803baa8b58ea1251
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-25 14:13:43 +0530

    fixed the .env file error

--- src/db/dbKey.config.js
+++ src/db/dbKey.config.js
@@ -1,6 +1,4 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const dbKey =
-  process.env.API_KEY ||
-  "mongodb+srv://MarkCarringotn:Carrington-115@cluster0.uqg27gz.mongodb.net/";
+require("dotenv").config({ path: "../../.env" });
+const dbKey = process.env.API_KEY;
+
 module.exports = { dbKey };
