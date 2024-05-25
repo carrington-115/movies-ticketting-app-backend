6e2ba26fe982b91139949f61b541e64d30747f96
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-25 14:05:51 +0530

    updated the api key and fixed the connection function "

--- src/db/dbKey.config.js
+++ src/db/dbKey.config.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const dbKey =
-  process.env.API_KEY |
+  process.env.API_KEY ||
   "mongodb+srv://MarkCarringotn:Carrington-115@cluster0.uqg27gz.mongodb.net/";
-console.log(dbKey);
 module.exports = { dbKey };
