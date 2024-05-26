adc7dfc77bbd0e2774637d99c2a0bb4ed9922b0f
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-26 09:36:25 +0530

    Run unit test for the filterMoviesWithDate function for year, lastupdated, and released and it ran perfectly

--- src/db/app.test.js
+++ src/db/app.test.js
@@ -2,13 +2,16 @@ const {
   getAllMovies,
   getMoviesWithId,
   filterMoviesByTitleAndGenres,
+  filterMoviesWithDate,
 } = require("./functions/movies");
 const { getAllUsers, getUsersByQuery } = require("./functions/users");
 
 const testFunc = async () => {
   try {
-    const movies = await filterMoviesByTitleAndGenres({
-      genres: ["Drama", "Adventure", "Fantasy", "Family", "Drama", "Horror"],
+    const movies = await filterMoviesWithDate({
+      year: 2000,
+      lastupdated: "2015-08-29 00:15:18.047000000",
+      released: "2000-02-18T00:00:00.000Z",
     });
     // console.log(movies);
     movies.forEach((movie) => console.log(movie));
