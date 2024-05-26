37e933e448dd8508b67b0f08609c3de9f00ba08c
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-26 11:28:08 +0530

    wrote three functions about the theaters getAllTheaters, getTheatersByAddress, getTheatersByGPS used session and aggregation for the operations

---
+++ src/db/functions/theaters.js
@@ -0,0 +1,82 @@
+const { client } = require("../client.config");
+const session = client.startSession();
+
+const getAllTheaters = async () => {
+  session.startTransaction();
+  try {
+    const theatersCollection = client.db("sample_mflix").collection("theaters");
+    const theaters = theatersCollection.aggregate([{ $limit: 50 }]);
+    await session.commitTransaction();
+    return theaters;
+  } catch (error) {
+    console.error(error);
+    await session.abortTransaction();
+  } finally {
+    await session.endSession();
+  }
+};
+
+const getTheatersByAddress = async (theaterAddress) => {
+  session.startTransaction();
+  const { street, city, zipcode, state } = theaterAddress;
+  try {
+    let theaters;
+    const theatersCollection = client.db("sample_mflix").collection("theaters");
+    if (street && city && zipcode && state) {
+      theaters = theatersCollection.aggregate([
+        {
+          $match: {
+            location: {
+              address: {
+                street1: street,
+                city: city,
+                zipcode: zipcode,
+                state: state,
+              },
+            },
+          },
+        },
+        { $limit: 50 },
+      ]);
+    } else {
+      theaters = theatersCollection.aggregate([{ $limit: 50 }]);
+    }
+    await session.commitTransaction();
+    return theaters;
+  } catch (error) {
+    console.error(error);
+    await session.abortTransaction();
+  } finally {
+    await session.endSession();
+  }
+};
+
+const getTheatersByGPS = async (theaterGPS) => {
+  session.startTransaction();
+  const { xCord, yCord } = theaterGPS;
+  try {
+    let theaters;
+    const theatersCollection = client.db("sample_mflix").collection("theaters");
+    if ((xCord, yCord)) {
+      theaters = theatersCollection.aggregate([
+        {
+          $match: {
+            geo: {
+              coordinates: { $in: [xCord, yCord] },
+            },
+          },
+        },
+      ]);
+    } else {
+    }
+    await session.commitTransaction();
+    return theaters;
+  } catch (error) {
+    console.error(error);
+    await session.abortTransaction();
+  } finally {
+    await session.endSession();
+  }
+};
+
+module.exports = { getAllTheaters, getTheatersByAddress, getTheatersByGPS };
